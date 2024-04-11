8a04ab4129329a76b6aca349b0722da389c87179
Farhad Dubey <farhaddubey003@gmail.com>
2024-04-11 17:16:42 +0530

    rebuilt is called

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,5 +1,7 @@
 import 'package:counter_app/cubit/counter_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -20,40 +22,46 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final counter = counterCubit.state; //Cause of two time init it failed to change the value hence to used the
+    // final counter = counterCubit.state; //Cause of two time init it failed to change the value hence to used the
     // instantiate of CounterCubit()
+    // Using that line we dont update the state. We just get one time value
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return BlocBuilder<CounterCubit, int>(
+      bloc: counterCubit,
+      // Now wherener the state is updated the Entire things under build is rebuilt
+      builder: (context, counter) {
+        return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            title: Text(widget.title),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'You have pushed the button this many times:',
+                ),
+                Text(
+                  // '$_counter',
+                  '$counter',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
             ),
-            Text(
-              // '$_counter',
-              '$counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        // onPressed: _incrementCounter,
-        onPressed: (){
-          counterCubit.increment();
-          setState(() {
-
-          });
-        },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          ),
+          floatingActionButton: FloatingActionButton(
+            // onPressed: _incrementCounter,
+            onPressed: (){
+              counterCubit.increment();
+              // setState(() {});
+              // If we don't want to use this setstate then We've to use the block builer mehtod
+            },
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+        );
+      }
     );
   }
 }
